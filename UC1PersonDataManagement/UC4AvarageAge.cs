4d83f9d76bc2e735f6eacec08952985c7128c4e5
Amit <amitkr24031995@gmail.com>
2021-10-18 18:55:04 +0530

    Adding UC4AvarageAge

---
+++ UC1PersonDataManagement/UC4AvarageAge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UC4AvarageAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> listperson = new List<Person>();
+            AddRecords(listperson);
+            RetrevingAvarageAge(listperson);
+        }
+        private static void AddRecords(List<Person> listperson)
+        {
+            listperson.Add(new Person("121212", "Amit", "12 main road", 14));
+            listperson.Add(new Person("222222", "Krishna", "15 main road", 13));
+            listperson.Add(new Person("333333", "Lala", "32 main road", 61));
+            listperson.Add(new Person("444444", "Sumit", "82 main road", 18));
+            listperson.Add(new Person("555555", "Satty", "98 main road", 24));
+        }
+        private static void RetrevingAvarageAge(List<Person> listperson)
+        {
+            double AvgAge = listperson.Average(x => x.Age);
+            Console.WriteLine("Average Age : " + AvgAge);
+        }
+    }
+}
